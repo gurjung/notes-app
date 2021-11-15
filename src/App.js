4e43b616e2397e95fdee9c1aa809ddc1e69aa4ed
gurjung <gurjung997@gmail.com>
2021-11-15 19:45:51 +0530

    adds dark mode

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { NotesList } from "./Components/NotesList.js";
 import { useState } from "react";
 import { nanoid } from "nanoid";
 import { Search } from "./Components/Search.js";
+import { MdDarkMode } from "react-icons/md";
+import { MdOutlineDarkMode } from "react-icons/md";
 function App() {
   const [notes, setNotes] = useState([
     { id: nanoid(), text: "Hello this is first note", date: "13/11/2021" },
@@ -12,6 +14,7 @@ function App() {
   ]);
 
   const [searchText, setSearchText] = useState("");
+  const [darkMode, setDarkMode] = useState(false);
   const addNote = (text) => {
     const today = new Date();
     const newNote = {
@@ -31,16 +34,32 @@ function App() {
   const handleSearchInput = (e) => {
     setSearchText(e.target.value);
   };
+
+  const handleToggleMode = () => {
+    setDarkMode(!darkMode);
+  };
   return (
-    <div className="container">
-      <Search handleSearchInput={handleSearchInput} />
-      <NotesList
-        notes={notes.filter((note) =>
-          note.text.toLowerCase().includes(searchText)
-        )}
-        handleAddNote={addNote}
-        handleDeleteNote={deleteNote}
-      />
+    <div className={`${darkMode ? "dark-mode" : "light-mode"}`}>
+      <div className="container">
+        <div className="container-header">
+          <h1 className={`${darkMode ? "header" : ""}`}>Notes</h1>
+          <div onClick={handleToggleMode}>
+            {darkMode ? (
+              <MdOutlineDarkMode size="2.3em" className="light-mode-icon" />
+            ) : (
+              <MdDarkMode size="2.3em" />
+            )}
+          </div>
+        </div>
+        <Search handleSearchInput={handleSearchInput} />
+        <NotesList
+          notes={notes.filter((note) =>
+            note.text.toLowerCase().includes(searchText)
+          )}
+          handleAddNote={addNote}
+          handleDeleteNote={deleteNote}
+        />
+      </div>
     </div>
   );
 }
